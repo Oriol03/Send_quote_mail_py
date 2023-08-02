e80cb5d55673eae4a70b6b521c5f4e228f86fe34
Oriol03 <140240772+Oriol03@users.noreply.github.com>
2023-08-02 17:12:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import smtplib
+import datetime as dt
+import random
+
+my_email= "owen23codeprof"
+mot_pass="emeibnxuukiurnhb"
+
+now=dt.datetime.now()
+week_day=now.weekday()
+if week_day%2==0:
+    with open("quotes.txt") as letter :
+        list_quotes=letter.readlines()
+        quote=random.choice(list_quotes)
+        print(quote)
+        with smtplib.SMTP("smtp.gmail.com") as connection :
+            connection.starttls()
+            connection.login(user=my_email, password=mot_pass)
+            connection.sendmail(from_addr=my_email,to_addrs="owenorioln@gmail.com",
+                                msg=f"Subject:Motivation speech\n\n{quote} ?")
+            connection.close()
